9bdcf747917fef09ac889f8689ba5688d0150681
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-05-03 13:59:43 +0200

    e

--- Library.cs
+++ Library.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bibliotek
+﻿namespace Bibliotek
 {
-    internal class Library
+    public class Library
     {
+
     }
 }
