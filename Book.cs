86a465c5d1a0e132ab8a3964de9c397e193ffe95
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-05-03 13:59:34 +0200

    g

---
+++ Book.cs
@@ -0,0 +1,6 @@
+﻿namespace Bibliotek
+{
+    public class Book
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var library = new Library();
+            var blablaBook = library.AddBook("Bla bla bla", 14); // 14 er lånetid
+            var abcBook = library.AddBook("Abc", 10);
+            var xyzBook = library.AddBook("Xyz", 20);
+            var per = library.AddLender("Per");
+            var pål = library.AddLender("Pål");
+
+            abcBook.RegisterLoan(per, DateTime.Now.AddDays(-15));
+            blablaBook.RegisterLoan(pål, DateTime.Now.AddDays(-5));
+
+            library.ShowNotLended();
+            library.ShowLendedExpired();
+            library.ShowLendedNotExpired();
+
+            per.ShowBooksAndDeadlines();
         }
     }
 }
